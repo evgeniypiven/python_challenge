e0f89ece8d93287d0dc1e394889610777db88fc0
Yevhenii <pivenevgeniy@ukr.net>
2024-10-05 02:25:44 +0300

    (add) postgresql database

--- python_challenge/settings.py
+++ python_challenge/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'python_challenge.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'python_challenge_db',
+        'USER': 'python_challenge_test',
+        'PASSWORD': 'python_challenge',
+        'HOST': 'postgres',
+        'PORT': 5432,
+        'ATOMIC_REQUESTS': True
     }
 }
 
